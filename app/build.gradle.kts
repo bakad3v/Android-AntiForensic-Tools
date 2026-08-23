c7bde13c13f4ad303cd45ade2b615df8006c58c8
bakad3v <162032863+bakad3v@users.noreply.github.com>
2026-08-23 13:50:51 +0300

    Version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.android.aftools"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "2.0.2"
+        versionCode = 11
+        versionName = "2.0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
